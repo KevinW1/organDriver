3276f806cbbdcd443b357174a065db41d5674a41
Kevin Whitfield <Kevin@Balance-of-Judgment-2.local>
2016-02-06 00:31:33 +1300

    code that actually works on the hardware

--- organDriver.ino
+++ organDriver.ino
@@ -1,126 +1,97 @@
 #include <MIDI.h>
 #include <string.h>
-
 MIDI_CREATE_DEFAULT_INSTANCE();
-
 //that old 74HC595N shift register
-#define latchPin 12  //pin 12 of chip
-#define clockPin 11  //pin 11 of chip
-#define dataPin 10  //pin 14 of chip
-#define outputEnable 5 //pin 13, inverted
-#define numChips 2  // number of shift registers
-#define noteOffset 24  //midi C1 value, start of rank
-#define noteLEDPin  4  //LED for blinking note light
+#define latchPin 11 //pin 12 of chip
+#define clockPin 10 //pin 11 of chip
+#define dataPin 12 //pin 14 of chip
+#define outputEnable 13 //pin 13, inverted
+#define numChips 8  // number of shift registers
+#define noteOffset 36 //midi C1 value, start of rank
+#define noteLEDPin 6 //LED for blinking note light
 
-#define DIP_LSB_PIN 9
-#define DIP_MSB_PIN 6
+#define dipPin1 2 //|pins for dip switch model 653-A6R-162RF
+#define dipPin2 5 //|
+#define dipPin4 4 //|
+#define dipPin8 3 //|
 
-byte chestChannel = 0x00;  // channel
-byte noteStates[numChips];  //note status array
-uint8_t activeNote = 0;  //for checking to see if any notes are on
+byte chestChannel = 0x00; // channel, 0 is omni
+byte noteStates[numChips]; //note status array
+uint8_t activeNote = 0; //for checking to see if any notes are on
+
 
 void setup()
 {
   pinMode(outputEnable, OUTPUT);
-  digitalWrite(outputEnable, HIGH);  //turn off outputs
+  digitalWrite(outputEnable, HIGH); //turn off outputs
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
   pinMode(noteLEDPin, OUTPUT);
 
-  //set the pinMode for the DIP switch
-  pinModeRange(DIP_LSB_PIN, DIP_MSB_PIN, INPUT_PULLUP);
+  pinMode(dipPin1, INPUT_PULLUP);
+  pinMode(dipPin2, INPUT_PULLUP);
+  pinMode(dipPin4, INPUT_PULLUP);
+  pinMode(dipPin8, INPUT_PULLUP);
 
-  //get the MIDI channel number from the DIP switch
-  chestChannel = readDIP(DIP_LSB_PIN, DIP_MSB_PIN, true);
+  setChannel();
 
-  memset(noteStates, 0, sizeof(noteStates));  // clear the note array
-
-  shiftShit();  //sets everything to zero
-
+  memset(noteStates, 0, sizeof(noteStates)); // clear the note array
+  shiftShit(); //sets everything to zero
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
   MIDI.begin(chestChannel);
-  digitalWrite(noteLEDPin, LOW);  //turn off note light
-  digitalWrite(outputEnable, LOW);  //turn on outputs
-}
-
-//sets a range of pins to a given mode
-void pinModeRange(uint8_t a, uint8_t b, int mode)
-{
-  //handle forwards and backwards ranges
-  uint8_t start = (a < b) ? a : b;
-  uint8_t stop  = (a < b) ? b : a;
-
-  for(uint8_t pin = start; pin <= stop; pin++)
-    pinMode(pin, mode);
+  digitalWrite(noteLEDPin, LOW); //turn off note light
+  digitalWrite(outputEnable, LOW); //turn on outputs
 }
 
 
-//works for DIP switches with a max of 8 bits
-uint8_t readDIP(uint8_t lsb_pin, uint8_t msb_pin, bool invert)
-{
-  uint8_t value = 0;
-
-  //handle big and little endian pin ranges
-  if(lsb_pin < msb_pin)
-  {
-    for(uint8_t pin = lsb_pin; pin <= msb_pin; pin++)
-      bitWrite(value, lsb_pin + pin, digitalRead(pin) ^ invert);
-  }
-  else if(msb_pin < lsb_pin)
-  {
-    for(uint8_t pin = lsb_pin; pin >= msb_pin; pin--)
-      bitWrite(value, lsb_pin - pin, digitalRead(pin) ^ invert);
-  }
-
-  return value;
+void setChannel() {
+  bitWrite(chestChannel, 0, !digitalRead(dipPin1));
+  bitWrite(chestChannel, 1, !digitalRead(dipPin2));
+  bitWrite(chestChannel, 2, !digitalRead(dipPin4));
+  bitWrite(chestChannel, 3, !digitalRead(dipPin8));
+  chestChannel += 1;  //because this library wants channel numbers 1-16, not 0-15
 }
 
 void loop()
 {
   MIDI.read();
-  shiftShit();
-} 
 
+  shiftShit();
+}
 void shiftShit()
 {
   activeNote = 0;
   digitalWrite(latchPin, LOW);
-  for(int i = 0; i < numChips; i++)
+  for (int i = 0; i < numChips; i++)
   {
-    shiftOut(dataPin, clockPin, MSBFIRST, noteStates[i]);  
+    shiftOut(dataPin, clockPin, MSBFIRST, noteStates[i]);
     activeNote += noteStates[i];
   }
   digitalWrite(latchPin, HIGH);
-  if(activeNote == 0){
+  if (activeNote == 0) {
     digitalWrite(noteLEDPin, LOW);
   }
-  else{
+  else {
     digitalWrite(noteLEDPin, HIGH);
   }
 }
-
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
-  updateNoteStatus(pitch, true);
+    updateNoteStatus(pitch, true);
 }
-
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
-  updateNoteStatus(pitch, false);
+    updateNoteStatus(pitch, false);
 }
-
 void updateNoteStatus(byte pitch, boolean state)
 {
   pitch -= noteOffset;
   pitch = (pitch < 0) ? 0 : pitch;
-  int chipNum = (pitch/8);
+  int chipNum = (pitch / 8);
   int pinNum = pitch % 8;
   bitWrite(noteStates[chipNum], pinNum, state);
 }
-
-
-
 
 
